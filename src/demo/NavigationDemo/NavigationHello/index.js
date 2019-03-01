230fc627b55ed25aa3e8184b2c3608c6635eee44
WangRui <maxieewong@gmail.com>
2019-03-01 19:33:48 +0800

    Nest router.

--- src/demo/NavigationDemo/NavigationHello/index.js
+++ src/demo/NavigationDemo/NavigationHello/index.js
@@ -1,6 +1,18 @@
 import React from "react";
-
+import { View } from 'react-native'
 import Markdown from "react-native-markdown-renderer";
+import { createMaterialTopTabNavigator } from 'react-navigation'
 import DocHelloNavigation from "../doc/DocHelloNavigation";
 
-export default (props) => <Markdown>{DocHelloNavigation}</Markdown>
+let DocComp = () => <View style={{margin: 10}}>
+    <Markdown>{DocHelloNavigation}</Markdown>
+</View>
+
+let CodeComp = () => <View style={{margin: 10}}></View>
+
+export default createMaterialTopTabNavigator({
+    Show: CodeComp,
+    Doc: DocComp
+})
+    
+    
--- src/demo/NavigationDemo/doc/DocHelloNavigation.js
+++ src/demo/NavigationDemo/doc/DocHelloNavigation.js
@@ -1,7 +1,13 @@
 export default `
-# Hello world!
+# Intro
 
-|a|b|
-|---|---|
-|c|d|
+Getting start React Navigation.
+
+This doc shows the core code of the page.
+
+# Install
+
+visit doc page: [https://reactnavigation.org/docs/en/getting-started.html](https://reactnavigation.org/docs/en/getting-started.html)
+
+
 `
