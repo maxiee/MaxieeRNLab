c157fb370f6e527f3fbd5a37ddb3772a8b45636f
WangRui <maxieewong@gmail.com>
2019-03-01 17:12:10 +0800

    HomePage new ui.

--- src/demo/NavigationDemo/index.js
+++ src/demo/NavigationDemo/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import { View } from "react-native";
 import { Text } from "react-native-elements";
 
-export default () => <View>
+export default (props) => <View>
     <Text h1>React Navigation Demo</Text>
 </View>
--- src/page/HomePage/index.js
+++ src/page/HomePage/index.js
@@ -2,25 +2,32 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Button } from "react-native-elements";
 
-export default (props) => <View style={styles.Container}>
-    <Text h3>React Native UI</Text>
-    <View style={styles.Section}>
-        <Button 
-            title="React Native Elements Demo"
-            onPress={() => props.navigation.navigate('ReactNavigation')}
-            type="outline" />
+export default class HomePage extends React.Component {
+    static navigationOptions = {
+        title: "MaxieeRNLab"
+    }
 
-    </View>
-    <Text h3>React Native Architecture</Text>
-    <View style={styles.Section}>
-        <Button 
-            title="React Navigation Demo"
-            onPress={() => props.navigation.navigate('ReactNavigation')}
-            type="outline" />
-
-    </View>
-    
-</View>
+    render() {
+        return (
+            <View style={styles.Container}>
+                <Text h3>React Native UI</Text>
+                <View style={styles.Section}>
+                    <Button 
+                        title="React Native Elements Demo"
+                        onPress={() => this.props.navigation.navigate('ReactNavigation')}
+                        type="outline" />
+                </View>
+                <Text h3>React Native Architecture</Text>
+                <View style={styles.Section}>
+                    <Button 
+                        title="React Navigation Demo"
+                        onPress={() => this.props.navigation.navigate('ReactNavigation')}
+                        type="outline" />
+                </View>
+            </View>
+        )
+    }
+}
 
 const styles = StyleSheet.create({
     Container: {
