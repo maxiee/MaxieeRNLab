927855713e6f3f8206c3c3af9e73a762fc796bac
maxiee <maxieewong@gmail.com>
2019-08-02 15:23:18 +0800

    update。

--- src/pages/basic/RNThirdSwiper.js
+++ src/pages/basic/RNThirdSwiper.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Text, View, ScrollView, StyleSheet, Button } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Button, Image } from 'react-native';
 import Swiper from 'react-native-swiper'
 import { createStackNavigator } from 'react-navigation';
+import GridView from '../../widgets/gridview/GridView';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const styles = StyleSheet.create({
     slide1: {
@@ -26,7 +28,12 @@ const styles = StyleSheet.create({
         color: '#fff',
         fontSize: 30,
         fontWeight: 'bold'
-    }
+    },
+    slide: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 })
 
 const BasicDemo = () => {
@@ -45,16 +52,102 @@ const BasicDemo = () => {
     )
 }
 
+const VerticalDemo = () => {
+    return (
+        <Swiper horizontal={false} autoplay>
+            <View style={styles.slide1}>
+                <Text style={styles.text}>Hello Swiper</Text>
+            </View>
+            <View style={styles.slide2}>
+                <Text style={styles.text}>Beautiful</Text>
+            </View>
+            <View style={styles.slide3}>
+                <Text style={styles.text}>And simple</Text>
+            </View>
+        </Swiper>
+    )
+}
+
+const ImageDemo = () => {
+    return (
+        <View style={{height: 240}}>
+            <Swiper height={240}
+                dot={
+                    <View style={{
+                        backgroundColor: 'rgba(0,0,0,.2)',
+                        width: 5,
+                        height: 5,
+                        borderRadius: 4,
+                        margin: 3
+                    }} />
+                }
+                activeDot={
+                    <View style={{
+                        backgroundColor: '#000',
+                        width: 8,
+                        height: 8,
+                        borderRadius: 4,
+                        margin: 3
+                    }} />
+                }
+                paginationStyle={{
+                    bottom: -23,
+                    left: null,
+                    right: 10
+                }}>
+                <View style={styles.slide} title={<Text numberOfLines={1}>Aussie tourist dies at Bali hotel</Text>}>
+                    <Image resizeMode='stretch' style={styles.image} source={require('./img/Swiper1.jpg')} />
+                </View>
+                <View style={styles.slide} title={<Text numberOfLines={1}>Big lie behind Nine’s new show</Text>}>
+                    <Image resizeMode='stretch' style={styles.image} source={require('./img/Swiper2.jpg')} />
+                </View>
+                <View style={styles.slide} title={<Text numberOfLines={1}>Why Stone split from Garfield</Text>}>
+                    <Image resizeMode='stretch' style={styles.image} source={require('./img/Swiper3.jpg')} />
+                </View>
+                <View style={styles.slide} title={<Text numberOfLines={1}>Learn from Kim K to land that job</Text>}>
+                    <Image resizeMode='stretch' style={styles.image} source={require('./img/Swiper4.jpg')} />
+                </View>
+            </Swiper>
+        </View>
+    )
+}
+
 const HomeDemo = (props) => {
+    let infos = [
+        {
+            title: "Basic",
+            subtitle: "",
+            icon: <Icon name="ios-albums" size={40} color="#999999" />,
+            onPress: () => props.navigation.push('Basic')
+        },
+        {
+            title: "Vertical",
+            subtitle: "autoplay",
+            icon: <Icon name="ios-menu" size={40} color="#999999" />,
+            onPress: () => props.navigation.push('Vertical')
+        },
+        {
+            title: "Image",
+            subtitle: "",
+            icon: <Icon name="ios-photos" size={40} color="#999999" />,
+            onPress: () => props.navigation.push('Image')
+        },
+    ];
 
     return (
-        <Button onPress={() => props.navigation.push('Basic')} title="Basic" />
+        <ScrollView>
+            <GridView
+                infos={infos}
+                onGridSelected={(index) => infos[index].onPress()}/>
+        </ScrollView>
     )
 }
 
 const Stack = createStackNavigator({
     Home: HomeDemo,
-    Basic: BasicDemo
+    Basic: BasicDemo,
+    Vertical: VerticalDemo,
+    Image: ImageDemo
 }, {
     headerMode: "none"
 });
